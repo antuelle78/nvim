22376b8674c754f4589bb7801e382edd9d6fdd1d
antuelle78 <miantuelle78@gmail.com>
2025-02-10 15:27:12 +0100

    Update colorscheme

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -110,11 +110,11 @@ return {
     }
   },
 
-  { "catppuccin/nvim",
-    event = "User AstroFile",
-    name = "catppuccin",
-    priority = 1000
-  },
+  -- { "catppuccin/nvim",
+  --   event = "User AstroFile",
+  --   name = "catppuccin",
+  --   priority = 1000
+  -- },
 
   -- == Examples of Adding Plugins ==
   vim.keymap.set("n", "<leader>z", "<cmd>%s/<C-R><C-W>/<C-R>0/g<cr>", { desc = "lazy_replace" }),
