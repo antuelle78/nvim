fd264e9b9174a6ae46c0e56e82e79637bdaa7aeb
antuelle78 <miantuelle78@gmail.com>
2025-10-26 19:10:52 +0100

    master blaster time

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,14 +11,14 @@ return {
     -- change colorscheme
     colorscheme = "catppuccin-mocha",
      -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-     highlights = {
-       init = { -- this table overrides highlights in all themes
-         Normal = { bg = "#313244" },
-       },
-       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-         Normal = { bg = "#313244" },
-       },
-     },
+     -- highlights = {
+     --   init = { -- this table overrides highlights in all themes
+     --     Normal = { bg = "NONE" },
+     --   },
+     --   astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+     --     Normal = { bg = "NONE" },
+     --   },
+     -- },
     -- Icons can be configured throughout the interface
     icons = {
       -- configure the loading of the lsp in the status line
