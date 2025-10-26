b744ddbd4d76974429bbd61934e0c11ef272399f
antuelle78 <miantuelle78@gmail.com>
2025-10-26 19:47:07 +0100

    Nice and smooth

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -31,6 +31,19 @@
         },
       },
     },
+    { "hashivim/vim-terraform" },
+    {
+      "Exafunction/windsurf.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+      },
+      config = function()
+        require("codeium").setup({
+          enable_cmp_source = true,
+        })
+      end,
+    },
     {
       "kdheepak/lazygit.nvim",
       lazy = true,
